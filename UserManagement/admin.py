1df0010303c92fbc25550670bd32443de32555e3
Ons <ons.diweni@esprit.tn>
2023-02-21 00:24:03 +0100

    updating

--- UserManagement/admin.py
+++ UserManagement/admin.py
@@ -3,9 +3,11 @@ from .models import *
 
 # Register your models here.
 
-
-#here we tell the admin that UserApp and Employe objects have an admin interface.
+#we are telling Django to include the CustomUser/Employee model in the Django Admin site.
+#@admin.register(Author)
 admin.site.register(CustomUser)
+admin.site.register(Employee)
+
 #admin.site.register(Employee)
 class UserAdmin(admin.ModelAdmin):
     pass
